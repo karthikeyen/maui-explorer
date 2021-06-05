08fec5ce0390d4d800c9e607e3335c7bde97d024
Karthik Krishna Baiju <karthikeyen.ktm@gmail.com>
2021-06-05 23:40:11 +0530

    battery level reading related changes

--- maui-samples-explorer/MainPage.xaml.cs
+++ maui-samples-explorer/MainPage.xaml.cs
@@ -1,21 +1,37 @@
-﻿using System;
-using Microsoft.Maui;
+﻿using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
 
 namespace maui_samples_explorer
 {
-	public partial class MainPage : ContentPage, IPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+    public partial class MainPage : ContentPage, IPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
 
-		int count = 0;
-		private void OnCounterClicked(object sender, EventArgs e)
-		{
-			count++;
-			CounterLabel.Text = $"Current count: {count}";
-		}
-	}
+            GetBatteryState();
+            Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
+        }
+
+        private void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            this.GetBatteryState();
+        }
+
+        private void GetBatteryState()
+        {
+            this.BatteryLevel.Text = "";
+
+            var state = Battery.State;
+            var source = Battery.PowerSource;
+
+            if (state != BatteryState.Discharging)
+            {
+                this.BatteryLevel.Text = $"{state} via {source.ToString()}";
+            }
+
+            labelPrecentage.Text = (Battery.ChargeLevel * 100).ToString();
+        }
+    }
 }
